a8da5f0f9213bfdc451a6211eba7c9f0c3327471
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-13 11:55:14 -0500

    exercise completed

---
+++ BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExercise
+{
+   public class BankAccount
+    {
+
+
+        
+
+        //Field = class local variables
+        private double balance = 0;
+
+
+       
+        //Method
+        public void Deposit (double amountToDeposit)
+        {
+            balance += amountToDeposit;
+        }
+
+
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAcct = new BankAccount();
+
+            Console.WriteLine("Please deposit your money now.");
+            double amount = double.Parse(Console.ReadLine());
+            
+            bankAcct.Deposit(amount);
+
+
+            Console.WriteLine($"Currently, you have {bankAcct.GetBalance(), 0:c} in your account.");
+           
+
+
+
+
+
+
+
+        }
+    }
+}
