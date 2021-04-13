6babf96086a36843aa3414f0cfad180449d70a09
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-13 13:09:36 -0500

    added notes

--- EncapsulationExercise/BankAccount.cs
+++ EncapsulationExercise/BankAccount.cs
@@ -9,14 +9,22 @@ namespace EncapsulationExercise
    public class BankAccount
     {
 
+        //In your BankAccount class, create the following:
+        //A private field of type double named balance with a value of 0
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+        //Define a method named GetBalance that will return the amount stored in the balance field
 
-        
+
 
         //Field = class local variables
         private double balance = 0;
 
 
-       
+        //Example of a Property
+        //public double Balance { get; set; }
+
+
+
         //Method
         public void Deposit (double amountToDeposit)
         {
@@ -24,7 +32,7 @@ namespace EncapsulationExercise
         }
 
 
-
+        //Method
         public double GetBalance()
         {
             return balance;
--- EncapsulationExercise/Program.cs
+++ EncapsulationExercise/Program.cs
@@ -6,7 +6,20 @@ namespace EncapsulationExercise
     {
         static void Main(string[] args)
         {
-            BankAccount bankAcct = new BankAccount();
+
+
+            //In your BankAccount class, create the following:
+            //A private field of type double named balance with a value of 0
+            //Define a method named Deposit that will accept a double and store that value in the balance field
+            //Define a method named GetBalance that will return the amount stored in the balance field
+            //In the Main() method of your application:
+            //Create a new instance of the BankAccount class
+            //Allow the user of the application to Deposit money
+            //Allow the user to retrieve their balance through the console
+            //Once finished save your work inside of Visual Studios
+
+
+            var bankAcct = new BankAccount();
 
             Console.WriteLine("Please deposit your money now.");
             double amount = double.Parse(Console.ReadLine());
